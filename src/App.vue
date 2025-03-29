181d05670eae18daf992e11125ec0a16640b5ada
William Lai <waihtgu@gmail.com>
2025-03-29 23:39:34 +0800

    messy code

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+const fun = () => {
+  console.log('i love')
+}
+
+fun()
+</script>
+
+<template>
+  <div class="box">
+    <h1>Me App</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.box {
+  width: 600px;
+  height: 400px;
+
+  h1 {
+    color: white;
+  }
+}
+
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  transition: filter 300ms;
+  will-change: filter;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
